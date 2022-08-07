174b08befaf437ca4a86d8b07d5ae5d2b446b67a
Parth <psp.11.jb@gmail.com>
2022-08-07 19:14:10 -0400

    updated the cart logic(updating the quantity)

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -34,29 +34,105 @@ function addItem(id) {
     var object = arrayOfObjects[i];
     if (object["id"] == id) {
       var oldCart = localStorage.getItem("cart");
-      console.log("cart type" + typeof oldCart);
-      var newCart = [
-        {
-          id: object["id"],
-          product_name: object["product_name"],
-          price: object["price"],
-          quantity: 1,
-        },
-      ];
-      console.log(typeof newCart);
+      // first phase when the cart is empty
       if (oldCart == "[]") {
-        console.log("First item added");
+        var newCart = [
+          {
+            id: object["id"],
+            product_name: object["product_name"],
+            price: object["price"],
+            quantity: 1,
+          },
+        ];
         var updateCartObject = [...newCart];
-        console.log(typeof updateCartObject);
+        console.log("Item added first time");
       } else {
-        console.log("second item");
-        var updateCartObject = [...eval(oldCart), ...newCart];
-        console.log(typeof updateCartObject);
+        console.log("Cart is not empty");
+        var oldCartobject = eval(oldCart);
+        var temp = false;
+        for (cartItem = 0; cartItem < oldCartobject.length; cartItem++) {
+          console.log("WHOLE CART", oldCartobject);
+          var oldCartItem = oldCartobject[cartItem];
+
+          if (oldCartItem["id"] == id) {
+            console.log("Duplicate Item Found");
+            var newCart = [
+              {
+                id: oldCartItem["id"],
+                product_name: oldCartItem["product_name"],
+                price: oldCartItem["price"],
+                quantity: oldCartItem["quantity"] + 1,
+              },
+            ];
+            const updatedOldCartObject = oldCartobject.filter(
+              (item) => item.id !== id
+            );
+            console.log("NEW one", updatedOldCartObject);
+
+            console.log("UPDATED WHOLE CART", oldCartobject);
+            // oldCartobject = [...newCart];
+            // console.log("oldCartobject", oldCartobject);
+            var updateCartObject = [...updatedOldCartObject, ...newCart];
+            console.log("done");
+            temp = true;
+          } else {
+            if (temp == false) {
+              console.log("New item added to the existing cart.");
+              var newCart = [
+                {
+                  id: object["id"],
+                  product_name: object["product_name"],
+                  price: object["price"],
+                  quantity: 1,
+                },
+              ];
+              var updateCartObject = [...eval(oldCart), ...newCart];
+            }
+          }
+
+          //   var newCart = [
+          //     {
+          //       id: oldCartItem["id"],
+          //       product_name: oldCartItem["product_name"],
+          //       price: oldCartItem["price"],
+          //       quantity: oldCartItem["quantity"] + 1,
+          //     },
+          //   ];
+          //   // var updateCartObject = [...eval(oldCart), ...newCart];
+
+          //   console.log("Duplicate Item Found");
+          // } else {
+          //   console.log("New item added to the existing cart.");
+          //   var newCart = [
+          //     {
+          //       id: object["id"],
+          //       product_name: object["product_name"],
+          //       price: object["price"],
+          //       quantity: 1,
+          //     },
+          //   ];
+          //   var updateCartObject = [...eval(oldCart), ...newCart];
+          // }
+        }
       }
+
+      // var newCart = [
+      //   {
+      //     id: object["id"],
+      //     product_name: object["product_name"],
+      //     price: object["price"],
+      //     quantity: 1,
+      //   },
+      // ];
+      // if (oldCart == "[]") {
+      //   var updateCartObject = [...newCart];
+      // } else {
+      //   var updateCartObject = [...eval(oldCart), ...newCart];
+      // }
       localStorage.setItem("cart", JSON.stringify(updateCartObject));
     }
+    var f = localStorage.getItem("cart");
+    console.log("Final Cart", f);
   }
-  var udpatedCart = localStorage.getItem("cart");
-  console.log("updated Cart" + udpatedCart);
-  updateCartNumber();
+  updateCartNumber(); // update the cart number in menu
 }
--- js/update-cart-number.js
+++ js/update-cart-number.js
@@ -1,6 +1,5 @@
 function updateCartNumber() {
   var cart = localStorage.getItem("cart");
   var cartObject = eval(cart);
-  console.log(cartObject.length);
   document.getElementById("cart-number").innerHTML = cartObject.length;
 }
